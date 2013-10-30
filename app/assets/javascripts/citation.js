b10c89d9e8c3327b72098fe1e8685c2978b47dd2
hdlnd <sparrow@sparrow.local>
2013-10-29 17:24:53 -0700

    Added Wikipedia citation format

--- app/assets/javascripts/citation.js
+++ app/assets/javascripts/citation.js
@@ -34,14 +34,15 @@ $(document).ready(function(){
                     var t_author_fullname = data.user.name;
                     var t_author_handle = data.user.screen_name;
                     var t_content = data.text;
-
+                    
+                //make 'created_at' more useful
                     var t_date_moment = moment.utc(t_date_raw, "ddd MMM DD HH:mm:ss ZZ YYYY");
-                    var mla_date = moment.utc(t_date_moment).format("DD MMM YYYY, HH:mm") + " UTC.";
 
-                        
-
-                //manipulate tweet data variables into variables for citations
+                //make name able to be reordered
                     var author_name_arr = t_author_fullname.split(' ');
+                
+                //variables needed for mla citation format
+                    var mla_date = moment.utc(t_date_moment).format("DD MMM YYYY, HH:mm") + " UTC.";
                     var mla_name = 0
                     if (author_name_arr.length > 2 || author_name_arr.length <= 1) {
                         mla_name = t_author_fullname+" ("+t_author_handle+"). ";
@@ -49,32 +50,41 @@ $(document).ready(function(){
                     else {
                         mla_name = author_name_arr[1]+", "+author_name_arr[0]+" ("+t_author_handle+"). ";
                     }
-                 
-                    var apa_name = t_author_handle+'. ';
-                    var t_date_arr = t_date_raw.split(' ');
-                    var t_time_arr = t_date_arr[3].split(':');
-                    var mla_time = t_time_arr[0]+':'+t_time_arr[1]+' UTC. ';
-                    //var mla_date = t_date_arr[2]+' '+t_date_arr[1]+' '+t_date_arr[5]+', '+mla_time;
-                    var apa_date = '('+t_date_arr[5]+', '+t_date_arr[1]+' '+t_date_arr[2]+'). ';
 
-                    var w_name = '"' + t_author_handle + tweet_id + '"';
-                    var w_title = '|title= Tweet Number ' + tweet_id;
-                    var w_url = '|url= ' + url;
-                    var w_author = '|author= ' + t_author_fullname;
-                    //var w_date = '|date= '+ 
+                //variables needed for apa citation format 
+                    var apa_date = moment.utc(t_date_moment).format("(YYYY, MMM DD).")
+                    var apa_name = t_author_handle+'. ';
+                   
+                //variables for Wikimedia citation format
+                    var w_name = '<ref name="' + t_author_handle + tweet_id + '">{{cite web';
+                    var w_title = ' |title= Tweet Number ' + tweet_id;
+                    var w_url = ' |url= ' + url;
+                    var w_author = ' |author= ' + t_author_fullname;
+                    var w_date = ' |date= '+ moment(t_date_moment).format('DD MMMM YYYY');
+                    var w_accessdate = '|accessdate= ' + moment().format('DD MMMM YYYY');
+                    var w_quote = ' |quote= ' + t_content;
+                    var w_work = ' |work= [[Twitter]] }}</ref>'     
+
+                //legacy code, remove after test
+                    //var t_date_arr = t_date_raw.split(' ');
+                    //var t_time_arr = t_date_arr[3].split(':');
+                    //var mla_time = t_time_arr[0]+':'+t_time_arr[1]+' UTC. ';
+                    //var mla_date = t_date_arr[2]+' '+t_date_arr[1]+' '+t_date_arr[5]+', '+mla_time;
+                    //var apa_date = '('+t_date_arr[5]+', '+t_date_arr[1]+' '+t_date_arr[2]+'). ';
                 
                 //create variables of citation strings for MLA and APA
                     mla_citation = mla_name+'"'+t_content+'". '+mla_date+" Tweet";
                     apa_citation = apa_name+apa_date+t_content+" [Twitter post]. "+"Retrieved from "+url;
                 
                 //create variables of citation strings for Wikipedia 
-                    //wiki_citation = 
+                    wiki_citation = w_name + w_title + w_url + w_author + w_date + w_accessdate + w_quote + w_work
 
                 //present the citations to the user
                     $('div#mla-citation').html(mla_citation);
                     $('div#apa-citation').html(apa_citation);
-                    $('#tweet-div').fadeIn(1500);
-                    $('#citations').fadeIn(2000);
+                    $('div#wiki-citation').html(wiki_citation);
+                    $('#tweet-div').show();
+                    $('#citations').show();
 
                 }
             });
